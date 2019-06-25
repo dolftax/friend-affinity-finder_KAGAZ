b31a73908d46ae95d288926eee63d5e78471ecb4
apoos-maximus <apoorv.99.sachan@gmail.com>
2019-06-25 14:25:34 +0530

    ping to kagaz apache !

---
+++ webapp/first.php
@@ -0,0 +1,14 @@
+<! DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "myfirst php script !" . "<br>"
+        ?>
+
+        <?php
+            $color = "red";
+            echo "My boat is " . $color. "<br>";
+            echo "My car is " . $COLOR. "<br>";
+        ?>
+    </body>
+</html>
